b809cd42549b58baa81dcf53b17038188743399b
mengo <menghong6988@gmail.com>
2023-12-06 09:59:54 +0800

    day4

--- day4/main.py
+++ day4/main.py
@@ -4,6 +4,7 @@ def main():
 	puzzle = get_input_lines("puzzle.txt")
 	# print(puzzle)
 	res = 0
+	count = [1] * len(puzzle)
 	for line in puzzle:
 		win, mine = line.split(":")[1].split("|")
 		win = [int(num) for num in win.split()]
@@ -11,9 +12,14 @@ def main():
 		value = 0
 		for num in win:
 			if num in mine:
-				value = 2*value if value is not 0 else 1
+				value = 2*value if value != 0 else 1
 		res += value
-
+	for i, line in enumerate(puzzle):
+		x,y = map(str.split, line.split('|'))
+		n = len(set(x) & set(y))
+		for j in range(i + 1, min(i+1+n,len(puzzle))):
+			count[j] += count[i]
+	print(sum(count))
 	print(res)
 		# print(card)
 	# print(games)
